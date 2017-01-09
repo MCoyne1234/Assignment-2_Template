c1847f67086518df8f8dc26f914ab3b349dc213c
Bryan Marsh <Bryan@Bryans-MacBook-Pro.local>
2017-01-09 00:37:27 -0800

    Added Template for Assignment 2

---
+++ assn2.asm
@@ -0,0 +1,44 @@
+;=================================================
+; Name: 
+; Email: 
+; 
+; Assignment name: Assignment 2
+; Lab section: 
+; 
+; I hereby certify that I have not received assistance on this assignment,
+; or used code, from ANY outside source other than the instruction team.
+;
+;=================================================
+
+.ORIG x3000			; Program begins here
+;-------------
+;Instructions
+;-------------
+
+;----------------------------------------------
+;outputs prompt
+;----------------------------------------------	
+LEA R0, intro			; 
+PUTS				; Invokes BIOS routine to output string
+
+;-------------------------------
+;INSERT CODE STARTING FROM HERE
+;--------------------------------
+
+
+
+HALT				; Stop execution of program
+;------	
+;Data
+;------
+; String to explain what to input 
+intro .STRINGZ	"ENTER two numbers (i.e '0'....'9')\n" 
+NEWLINE .STRINGZ "\n"	; String that holds the newline character
+
+
+
+;---------------	
+;END of PROGRAM
+;---------------	
+.END
+
